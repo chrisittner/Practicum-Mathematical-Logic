ce6811f9221a9cf8f6dd8b01a6a952fbc709c16c
chrisittner <mail@chrisittner.de>
2017-06-19 10:01:34 +0200

    extended ch3

--- chapter-3/propositional-logic.scm
+++ chapter-3/propositional-logic.scm
@@ -6,6 +6,8 @@
 ;; Propositional logic with "->" primitive & further defined connectives ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; (all connectives are in prefix notation)
+
 (define (is-implication? formula)
   (and (list? formula)
        (= (length formula) 3)
@@ -13,53 +15,75 @@
        (is-formula? (cadr formula))
        (is-formula? (caddr formula))))
 
-
-
-
-(define (is-formula? formula)
-  (or (is-variable? formula) (is-implication? formula)))
-
-
-
-;; defining new sentential connectives/operators ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define (BLOCKED-SYMBOLS) '())
-
+;; global variable storing currently defined sentential connectives/operators
 (define CONNECTIVES '())
+;; each connective is a triple (name-symbol arity introduction-clauses) where
+;;   - arity is the list of arguments for the connective
+;;   - introduction-clauses is a list of formulas of shape
+;;         (some-formula -> (new-connective ..))
+
 (define (_ARITY name)     (2nd (assoc name CONNECTIVES)))
 (define (_I-CLAUSES name) (3rd (assoc name CONNECTIVES)))
 
-(define (add-connective name arity i-clauses)
-  (if #t ;; check form + strict positivity
+(define (is-defined-connective? formula)
+  (and (list? formula)
+       ;; (1) is in CONNECTIVES
+       (member? (1st formula) (map car CONNECTIVES))
+       ;; (2) has correct number of arguments
+       (= (length formula) (+ 1 (length (_ARITY (1st formula)))))
+       ;; (3) each argument is a formula
+       (and (map is-formula? (cdr formula)))))
+
+(define (is-formula? formula)
+  (or (is-variable? formula)
+      (is-implication? formula)
+      (is-defined-connective? formula)))
+
+
+;; Extend the language by with a defined connective
+;; e.g. (add-connective '& '(A B) '((-> A (-> B (& A B)))))
+;;   - arity is the list of arguments for the connective
+;;   - i-clauses is a (non-empty) list of introduction clauses of shape
+;;        (some-formula -> (new-connective ..)
+;;     the new connective may only appear "strictly positive" in 'some-formula.
+(define (define-connective name arity i-clauses)
+  (if #t ;; TODO: check form + strict positivity
       (set! CONNECTIVES
             (append (list name arity i-clauses) CONNECTIVES))
-      (error "bla")))
+      (error "ERROR: not a valid definition")))
 
-;; this (1) extends the formula language by the connective 'name' and
-;;      (2) extends the proof term language by functions symbols 'name+_n' and 'name-'
-;; TODO: update is-formula, is-derivation, check-derivation
+
+(define-connective 'neg  '(A)   '((-> (-> A bot) (neg A))))
+(define-connective '&    '(A B) '((-> A (-> B (& A B)))))
+(define-connective 'v    '(A B) '((-> A (v A B))
+                                  (-> B (v A B))))
+;(define-connective 'weak-v  '(A B) '(-> (neg (-> (neg A) (neg B))) (weak-v A B)))
+
+
 ;; TODO: -display current connectives (formula language)
 ;;       -display current rules/term languag
 
-;; for PDF: add consistency, normalisation, conservativity proof, if applicable
 
 ;; returns the general elimination rule for the given connective,
 ;; derived from its introduction rules
 (define (elimination-clause name)
   (let ((evar (gen-var "C" (_ARITY name))))
     (list '-> (append (list name) (_ARITY name))
-          (list '-> ;competitor-satisfies-clauses
-                (append (list evar) (_ARITY name)))))
+          (list '-> ;; TODO: express "the competitor evar satisfies all i-clauses"
+                (append (list evar) (_ARITY name))))))
 
-(add-connective 'neg  '(A)   '((-> (-> A bot) (neg A))))
-(add-connective 'lor  '(A B) '((-> A (lor A B))
-                               (-> B (lor A B))))                               
-(add-connective 'land '(A B) '((-> A (-> B (land A B)))))
-;(add-connective 'weak-or  '(A B) '(-> (neg (-> (neg A) (neg B))) (lor A B)))
+                               
 
 
 
+;; EXERCISE 1: extend the proof term language:
+;;   for each defined connective 'name, accept functions symbols
+;;      'name+_n' and 'name-'
+;;   in derivation terms. They express the n-th introduction rule or the elimination rule
+;;   for the defined connective. Their type is given by the i-clauses of 'name.
+
+;; (a) is-valid-derivation? needs to check whether these newly defined term symbols are used type-correctly
+;; (b) infer-formula similarily needs to support the new rules.
 
 
 
@@ -97,91 +121,6 @@
        (is-formula? formula)
        (equal? (infer-formula term context) formula)))
       
-
-
-
-
-
-;; Interactive helper for bottom-to-top derivation construction
-
-;; We need the following global state triple:
-;; (1) a goal formula
-;; (2) a partial derivation term (a derivation term with '?_1' variables)
-;; (3) a list of open subgoals, their formula types and their contexts, the first one being currently active
-(define PPROOF-STATE '())
-
-(define (_GOAL)     (1st PPROOF-STATE))
-(define (_PPROOF)   (2nd PPROOF-STATE))
-(define (_SUBGOALS) (3rd PPROOF-STATE))
-
-(define (_CURGOAL-VAR) (1st (1st (_SUBGOALS))))
-(define (_CURGOAL)     (2nd (1st (_SUBGOALS))))
-(define (_CONTEXT)     (3rd (1st (_SUBGOALS))))
-
-;; TODO
-(define (print-proof-state)
-  ;; print context
-  ;; print current subgoal)
-  '())
-
-(define (set-goal formula context)
-  (begin (set! PPROOF-STATE (list formula '?_0 (list '?_0 context)))
-         (print-proof-state)))
-
-;; moves antecedent(s) of current subgoal into context.
-;; requires subgoal to be of (nested) implication form.
-;; takes one or more names.
-(define (assume name . more-names)
-    (if (not (is-implication? (_CURGOAL)))
-        (error "cannot assume: active subgoal not in implication form"))
-        (let* ((antecedent  (2nd (_CURGOAL)))
-               (consequent  (3rd (_CURGOAL)))
-               (new-subgoal consequent)
-               (new-context (append (list name antecedent) (_CONTEXT)))
-               (new-pproof  (substitute (_PPROOF)
-                                (_CURGOAL-VAR)
-                                (list 'lambda (list name) (_CURGOAL-VAR)))))
-          (begin
-            (set! PPROOF-STATE
-                  (list (_GOAL)
-                        new-pproof
-                        (append (list (_CURGOAL-VAR) new-subgoal new-context)
-                                (cdr (_SUBGOALS)))))
-            (if (not (null? more-names))
-                (apply assume more-names)
-                (print-proof-state)))))
-        
-;; use
-;; intro
-;; elim
-;; inst
-  
-;(define (use avar)
-;  ;; check if (1) in context or (2) a saved proof
-;  (let ((aformula (cdr (assoc avar (_CONTEXT)))))
-;    (cond
-;      ((and (not (equal? (_CURGOAL) aformula)) (is-implication? aformula))
-;       (do (              ;create new subgoal for antecedent 
-;             )
-;         (and (not (equal? (_CURGOAL) aformula)) (is-implication? aformula))))
-;      ((equal? (_CURGOAL) aformula)
-;          (set! PPROOF-STATE (list (_GOAL)
-;                                   (substitute (_PPROOF) (_CURGOAL-VAR) avar)
-;                                   (cdr (_SUBGOALS))))
-;          ; print update and proof-state "ok, can be obtained from..."
-;          )
-;      (else (error "cannot use..")))))
-
-(define THEOREMS '())
-
-(define (save-proof name)
-  (if #f ;proof is finished
-      (begin
-        (set! THEOREMS (append (list name (_GOAL) (_PPROOF)) THEOREMS))
-        ; print saved...
-        )
-      (error "cannot save an unfinished proof")))
-
 
 
 
